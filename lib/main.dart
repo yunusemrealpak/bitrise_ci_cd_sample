f7d2ef01e5ed4364279c52d53cbdb24d36771fd1
Yunus Emre Alpak <48226433+yunusemrealpak@users.noreply.github.com>
2022-06-17 15:49:34 +0300

    firebase integrated

--- lib/main.dart
+++ lib/main.dart
@@ -103,7 +103,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Güncel Hal',
             ),
             Text(
               '$_counter',
